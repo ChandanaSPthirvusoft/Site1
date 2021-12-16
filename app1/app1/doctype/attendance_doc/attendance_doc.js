bb1a29c89173bcaca6603d78505060cf876e9cf1
ChandanaSPthirvusoft <chandan@thirvusoft.in>
2021-12-16 13:12:12 +0530

    doctype created

---
+++ app1/app1/doctype/attendance_doc/attendance_doc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, pub and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Attendance_doc', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ app1/app1/doctype/attendance_doc/test_attendance_doc.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, pub and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAttendance_doc(unittest.TestCase):
+	pass
